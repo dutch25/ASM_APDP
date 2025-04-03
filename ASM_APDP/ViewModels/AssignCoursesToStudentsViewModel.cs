2dc96bdea8707a8c3b50b81dbfac5ace04910eb2
datpro-sudo <nguyentiendat.qc2004@gmail.com>
2025-04-03 12:11:40 +0700

    done MANAGE ADD STUDENT IN CLASS

--- ASM_APDP/ViewModels/AssignCoursesToStudentsViewModel.cs
+++ ASM_APDP/ViewModels/AssignCoursesToStudentsViewModel.cs
@@ -6,25 +6,13 @@ namespace ASM_APDP.ViewModels
 {
     public class AssignCoursesToStudentsViewModel
     {
-        [Display(Name = "Học sinh")]
-        [Required(ErrorMessage = "Vui lòng chọn học sinh")]
+        public int ClassId { get; set; } // Original ClassId
+        public int NewClassId { get; set; } // New ClassId from dropdown
         public int StudentId { get; set; }
-
-        public string StudentName { get; set; } // Để hiển thị thông tin (không cần nhập)
-
-        [Display(Name = "Lớp học")]
-        [Required(ErrorMessage = "Vui lòng chọn lớp học")]
-        public int ClassId { get; set; } // Sử dụng ClassId để chọn lớp hiện có
-
-        public string ClassName { get; set; } // Để hiển thị thông tin (không cần nhập)
-
-        [Display(Name = "Khóa học")]
-        [Required(ErrorMessage = "Vui lòng chọn khóa học")]
+        public string ClassName { get; set; }
         public int CourseId { get; set; }
-
-        // Danh sách để hiển thị trong dropdown
-        public List<User> Students { get; set; } = new List<User>();
-        public List<Class> Classes { get; set; } = new List<Class>();
-        public List<Course> Courses { get; set; } = new List<Course>();
+        public List<User> Students { get; set; }
+        public List<Class> Classes { get; set; }
+        public List<Course> Courses { get; set; }
     }
 }
